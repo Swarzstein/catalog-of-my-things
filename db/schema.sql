f61506ef27316bf780fe84fbac51b6441f21b72b
Swarzstein <zorrillagja@gmail.com>
2023-04-05 12:50:20 -0400

    schema.sql updated

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,6 @@
 CREATE DATABASE catalog;
 \c catalog
 
-CREATE TABLE item (
-    id SERIAL PRIMARY KEY NOT NULL,
-    publish_date DATE NOT NULL,
-    archived BOOLEAN,
-    genre_id INTEGER,
-    author_id INTEGER,
-    label_id INTEGER
-);
-
 CREATE TABLE book (
     id SERIAL PRIMARY KEY NOT NULL,
     publish_date DATE NOT NULL,
@@ -27,5 +18,4 @@ CREATE TABLE label (
     color VARCHAR (200)
 );
 
-ALTER TABLE item ADD CONSTRAINT fk_label FOREIGN KEY(label_id) REFERENCES label(id);
 ALTER TABLE book ADD CONSTRAINT fk_label FOREIGN KEY(label_id) REFERENCES label(id);
