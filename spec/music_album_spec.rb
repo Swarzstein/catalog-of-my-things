f970ff337db0977f7c7e479148531bd7f5361cf3
Israel <chizunguisrael@gmail.com>
2023-04-05 15:21:34 +0200

    Perform unit test for music album

---
+++ spec/music_album_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../classes/music_album'
+
+describe 'Test Music Album class' do
+  album = MusicAlbum.new('2023-12-10', 'Y')
+
+  it 'takes parameters and returns an album object' do
+    expect(album).to be_instance_of(MusicAlbum)
+  end
+
+  it "game publish date should be '2023-12-10'" do
+    expect(album.publish_date).to eq Date.new(2023, 12, 10)
+  end
+end
